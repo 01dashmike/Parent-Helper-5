56418a4610cfa928904eaba3415ceef7b1e1f81a
01dashmike <mikejchilton@icloud.com>
2025-12-06 09:53:02 +0000

    fix: add ESLint config file for CI

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-case-declarations': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'warn',
+  },
+};
